979d09f7d73f115c0370bb89988918e26bc88596
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-06-04 19:12:35 -0300

    Bean do Carro

---
+++ src/main/java/org/traveller/bean/CarroBean.java
@@ -0,0 +1,39 @@
+package org.traveller.bean;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+
+import org.traveller.dao.CarroDAO;
+import org.traveller.model.Carro;
+
+@Named
+@RequestScoped
+public class CarroBean {
+	
+	Carro carro = new Carro();
+	
+	
+	public CarroBean() {
+		super();
+	}
+
+	public CarroBean(Carro carro) {
+		super();
+		this.carro = carro;
+	}
+
+	
+	public Carro getCarro() {
+		return carro;
+	}
+
+	public void setCarro(Carro carro) {
+		this.carro = carro;
+	}
+
+	public List<Carro> getUsers() {
+		return new CarroDAO().getAllCarros();
+	}
+}
