526dcdf26d1efa80ae79f4aeb42d568c5158c7cc
CarvalhoGabriell <gabriel.fernandes2312@gmail.com>
2021-06-04 19:14:15 -0300

    Endpoint CRUD via REST do carro

--- src/main/java/org/traveller/rest/RestApplication.java
+++ src/main/java/org/traveller/rest/RestApplication.java
@@ -3,6 +3,6 @@ package org.traveller.rest;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.ApplicationPath;
 
-@ApplicationPath("/rest")
+@ApplicationPath("/api")
 public class RestApplication extends Application {
 }
